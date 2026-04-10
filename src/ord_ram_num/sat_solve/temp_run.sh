ea685f5a7df9c695c06560b9384eb4dd4108a11d
Nino Bašić <nino.basic@gmail.com>
2026-04-10 19:07:51 +0200

    Filenames contain leading zeros

--- src/ord_ram_num/sat_solve/temp_run.sh
+++ src/ord_ram_num/sat_solve/temp_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-maxtimegen="50s"
-maxtimekis="5s"
+maxtimegen="1m"
+maxtimekis="2m"
 type1="pmon"
 type2="pmon"
 mode="cyc"
@@ -9,8 +9,12 @@ outfile="../../../kissat_output/${type1}_${type2}_${mode}/${type1}${1}_${type2}$
 
 n=$(( $1 > $2 ? $1 : $2 ))
 
+f1=$(printf "%02d" "${1}")
+f2=$(printf "%02d" "${2}")
+
 while true; do
-	outfile="../../../kissat_output/${type1}_${type2}_${mode}/${type1}${1}_${type2}${2}_${mode}_${n}.txt"
+	fn=$(printf "%02d" "${n}")
+	outfile="../../../kissat_output/${type1}_${type2}_${mode}/${type1}${f1}_${type2}${f2}_${mode}_${fn}.txt"
 	g++ -O2 cnf_gen_main.cpp -o cnf_gen_main || exit 1
 	timeout $maxtimegen ./cnf_gen_main tmp.cnf $n $mode $1 $type1 $2 $type2
 	if [ $? -eq 124 ]; then
